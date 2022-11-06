2f45abc4c6d268b04ff17434ddcbc04da0ee9061
Saggressive <919071795@qq.com>
2022-11-06 21:58:27 +0800

    base

---
+++ copy.sh
@@ -0,0 +1,18 @@
+cp -r ../beit2_ratio_copy/__pycache__ ./
+cp -r ../beit2_ratio_copy/.vscode ./
+cp -r ../beit2_ratio_copy/all_dir ./
+cp -r ../beit2_ratio_copy/args ./
+cp -r ../beit2_ratio_copy/condenser ./
+cp -r ../beit2_ratio_copy/debug ./
+cp -r ../beit2_ratio_copy/demo ./
+cp -r ../beit2_ratio_copy/img1k_demo ./
+cp -r ../beit2_ratio_copy/ir_data ./
+cp -r ../beit2_ratio_copy/log_dir ./
+cp -r ../beit2_ratio_copy/pretrained_model ./
+cp -r ../beit2_ratio_copy/py_scripts ./
+cp -r ../beit2_ratio_copy/semantic_segmentation ./
+cp -r ../beit2_ratio_copy/SentEval ./
+cp -r ../beit2_ratio_copy/vqkd_teacher ./
+cp ../beit2_ratio_copy/*.py ./
+cp ../beit2_ratio_copy/*.sh ./
+ln -s /share/a100/vig_pytorch/imagenet-2012 ./
